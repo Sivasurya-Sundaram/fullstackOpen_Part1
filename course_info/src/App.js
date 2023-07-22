ebeefad82aa5bff8c576332f33b191f1852edb31
Sivasurya Sundaram <gamerganapathi@live.com>
2023-07-22 16:30:52 +0530

    exercise_1.5

--- course_info/src/App.js
+++ course_info/src/App.js
@@ -34,21 +34,23 @@ const Part=(props)=>{
   )
 }
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   // const part1 = "Fundamentals of React";
   // const exercises1 = 10;
   // const part2 = "Using props to pass data";
@@ -58,9 +60,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
       {/* <h1>{course}</h1>
       <p>
         {part1} {exercises1}
